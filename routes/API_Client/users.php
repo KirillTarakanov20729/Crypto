dfcf60f49c4ab21c2607c53bc68bc594efc74697
KirillTarakanov20729 <kirill.tarakanov00@mail.ru>
2024-07-25 22:13:47 +0300

    Fix Routes Again :(

--- routes/API_Client/users.php
+++ routes/API_Client/users.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->group(function () {
-    Route::get('index', [\App\Http\Controllers\API_Client\User\UserController::class, 'index']);
+    Route::post('index', [\App\Http\Controllers\API_Client\User\UserController::class, 'index']);
 });
