1daef544b2d4d50c30497ac53c83297d4396e80c
KirillTarakanov20729 <kirill.tarakanov00@mail.ru>
2024-08-09 16:22:13 +0300

    Fix

--- database/seeders/WalletSeeder.php
+++ database/seeders/WalletSeeder.php
@@ -22,28 +22,28 @@ class WalletSeeder extends Seeder
 
             $btc_wallet = new Wallet;
             $btc_wallet->user_id = $user->id;
-            $btc_wallet->coin_id = 1;
+            $btc_wallet->coin_id = 4;
             $btc_wallet->balance = rand(1, 100);
             $btc_wallet->uuid = uuid_create();
             $btc_wallet->save();
 
             $eth_wallet = new Wallet;
             $eth_wallet->user_id = $user->id;
-            $eth_wallet->coin_id = 2;
-            $eth_wallet->balance = rand(5, 150);
+            $eth_wallet->coin_id = 1;
+            $eth_wallet->balance = rand(1, 100);
             $eth_wallet->uuid = uuid_create();
             $eth_wallet->save();
 
             $sol_wallet = new Wallet;
             $sol_wallet->user_id = $user->id;
-            $sol_wallet->coin_id = 3;
+            $sol_wallet->coin_id = 2;
             $sol_wallet->balance = rand(100, 1000);
             $sol_wallet->uuid = uuid_create();
             $sol_wallet->save();
 
             $bnb_wallet = new Wallet;
             $bnb_wallet->user_id = $user->id;
-            $bnb_wallet->coin_id = 4;
+            $bnb_wallet->coin_id = 3;
             $bnb_wallet->balance = rand(20, 400);
             $bnb_wallet->uuid = uuid_create();
             $bnb_wallet->save();
