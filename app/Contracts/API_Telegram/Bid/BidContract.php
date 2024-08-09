f44f89048e8d22b41ddb47e4d1784597bf590362
KirillTarakanov20729 <kirill.tarakanov00@mail.ru>
2024-08-09 22:20:15 +0300

    Fix Again

--- app/Contracts/API_Telegram/Bid/BidContract.php
+++ app/Contracts/API_Telegram/Bid/BidContract.php
@@ -4,10 +4,8 @@ namespace App\Contracts\API_Telegram\Bid;
 
 use App\DTO\API_Telegram\Bid\IndexDTO;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Database\Eloquent\Collection;
 
 interface BidContract
 {
-    public function index(IndexDTO $data): Collection;
+    public function index(IndexDTO $data): LengthAwarePaginator;
 }
--- app/Services/API_Telegram/Bids/BidService.php
+++ app/Services/API_Telegram/Bids/BidService.php
@@ -9,17 +9,16 @@ use App\Http\Filters\BidFilter;
 use App\Models\Bid;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
 
 class BidService implements BidContract
 {
-    public function index(IndexDTO $data): Collection
+    public function index(IndexDTO $data): LengthAwarePaginator
     {
         $filter = app()->make(BidFilter::class, ['queryParams' => $data->except('page')->toArray()]);
 
         try {
-            return Bid::filter($filter)->with(['user', 'coin', 'currency'])->simplePaginate(3, ['*'], 'page', $data->page)->except('meta');
+            return Bid::filter($filter)->with(['user', 'coin', 'currency'])->paginate(3, ['*'], 'page', $data->page);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             throw new IndexBidsException('Something went wrong', 500);
